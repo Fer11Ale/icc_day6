d97bddea33ad4c1b4f01b4ab5f7282f533a47c5a
Fer11Ale <Fcont32622@my.sunywcc.edu>
2025-02-28 11:57:12 -0500

    Add files via upload

---
+++ minigameinclass.py
@@ -0,0 +1,19 @@
+# we meed the random "library"
+from random import randrange
+
+# makes a number between 0 and 100 (excluding 100)
+# and saves it into a variablecalled "answer"
+answer = randrange(100000) 
+
+#run the code inside of the while loop forever
+while True:
+    #ask the player for a number and save it
+    player_guess = int(input("Guess a number: "))
+    if (answer == player_guess): 
+        print("You win!") # print win message
+        break # end the game by breaking the loop
+    
+    if (player_guess > answer):
+        print("It's smaller")  
+    else:
+        print("It's bigger")  
